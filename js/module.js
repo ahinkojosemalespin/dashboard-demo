83e7c55c260fbfca503ed11d2c488668fda976ae
ahinkojosemalespin <jose.malespin@ahinko.com>
2014-03-31 17:39:10 -0600

    Added angularjs and a nodejs server
    Run the app.js file in node to run the server. This runs at port 3000.
    When start, go to localhost:3000/index.html

---
+++ js/module.js
@@ -0,0 +1,43 @@
+﻿var conversionomicsApp = angular.module('conversionomicsApp', [
+  'ngRoute',
+  'conversionomicsControllers'
+]);
+
+conversionomicsApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/dashboard', {
+            templateUrl: 'dashboard.html',
+            controller: 'dashboardController'
+        }).
+        when('/clients', {
+             templateUrl: 'clients.html',
+             controller: 'PhoneDetailCtrl'
+        }).
+        when('/google_analytics', {
+             templateUrl: 'google_analytics.html',
+             controller: 'PhoneDetailCtrl'
+        }).
+        when('/google_adwords', {
+            templateUrl: 'google_adwords.html',
+            controller: 'PhoneDetailCtrl'
+        }).
+        when('/bing', {
+            templateUrl: 'bing.html',
+            controller: 'PhoneDetailCtrl'
+        }).
+        when('/queries', {
+            templateUrl: 'queries.html',
+            controller: 'PhoneDetailCtrl'
+        }).
+        otherwise({
+            redirectTo: '/dashboard'
+        });
+  }]);
+
+var conversionomicsControllers = angular.module('conversionomicsControllers', []);
+
+conversionomicsControllers.controller('dashboardController', ['$scope', '$http',
+  function ($scope, $http) {
+      $scope.title = "Dashboard"
+  }]);
